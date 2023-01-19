d95bc7796af8fa8b1b5b4c9e72f901e065e36e8b
FrancieleAires <f.aires2016@gmail.com>
2023-01-19 17:31:26 -0300

    Adicionar arquivos de projeto.

---
+++ SalarioBonus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double TOTAL;
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalDinheiro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            TOTAL = (totalDinheiro * 15 / 100) + salario;
+           
+
+            Console.WriteLine("TOTAL = R$ " + TOTAL.ToString("F2",CultureInfo.InvariantCulture));
+           
+        }
+    }
+}
